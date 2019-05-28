2bd3a6d9c59d99cff794569b39ca9d8d378785d3
Mohd Zuhair Mohd Radzi <32242267+zuhairradzi@users.noreply.github.com>
2019-05-28 15:58:07 +0800

    Find Element Commands

--- SeleniumTutorial/NunitTest.cs
+++ SeleniumTutorial/NunitTest.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using NUnit.Framework;
+using System.Threading;
 
 namespace SeleniumTutorial
 {
     class NunitTest
     {
         IWebDriver driver;
+        IWebElement element;
 
         [SetUp]
         public void Initialize()
@@ -72,6 +74,110 @@ namespace SeleniumTutorial
             driver.Navigate().Refresh();
 
         }
+
+        [Test, Order(4)]
+        public void test4_ElementCommands()
+        {
+            driver.Url = "https://www.google.com/";
+
+            //sendtext and clear
+            driver.FindElement(By.XPath("//*[@id='tsf']/div[2]/div/div[1]/div/div[1]/input")).SendKeys("test");
+            driver.FindElement(By.XPath("//*[@id='tsf']/div[2]/div/div[1]/div/div[1]/input")).Clear();
+
+            driver.Navigate().GoToUrl("https://www.demoqa.com");
+
+            //click element
+            driver.FindElement(By.XPath("//*[@id='sidebar']/aside[1]/ul/li[2]/a")).Click();
+
+            //check if element enabled, display and selected
+            element = driver.FindElement(By.XPath("//*[@id='selectable']/li[1]"));
+            bool enabled = element.Enabled;
+            bool displayed = element.Displayed;
+            Assert.That(enabled.Equals(true));
+            Assert.That(displayed.Equals(true));
+
+            //check if element selected
+            driver.FindElement(By.XPath("//*[@id='sidebar']/aside[2]/ul/li[11]/a")).Click();
+            element = driver.FindElement(By.XPath("//*[@id='content']/div[2]/div/fieldset[1]/label[1]/span[1]"));
+            element.Click();
+            Thread.Sleep(2000);
+            bool selected = element.Selected;
+            //Assert.That(selected.Equals(true));
+
+            //get tag name
+            string tagname = element.TagName;
+            Console.WriteLine("Tag name : "+ tagname);
+
+            //get attribute
+            string attribute = element.GetAttribute("class");
+            Console.WriteLine("attribute name : " + attribute);
+
+            //get css value
+            string css = element.GetCssValue("radio-1");
+            Console.WriteLine("css : " + css);
+
+            //get type of element
+            element.GetType();
+            Console.WriteLine("element.GetType(); : " + element.GetType());
+
+
+
+        }
+
+        [Test, Order(5)]
+        public void test5_findElementsCommands()
+        {
+            driver.Url = "http://toolsqa.com/automation-practice-form/";
+
+            //partial link text
+            driver.FindElement(By.PartialLinkText("Partial")).Click();
+
+            //link text
+            driver.FindElement(By.LinkText("Link Test")).Click();
+            driver.Navigate().Back();
+
+            //name
+            //enter name
+            driver.FindElement(By.Name("firstname")).SendKeys("zuzu");
+
+            //id
+            driver.FindElement(By.Id("sex-0")).Click();
+
+            //css selector
+            //select 1 year exp level
+            driver.FindElement(By.CssSelector("input[id='exp-0']")).Click();
+
+            //collect all exp level radio button
+            IList<IWebElement> expLevelCheck = driver.FindElements(By.Name("exp"));
+
+            //check if button radio selected
+            bool isExpCheck;
+            isExpCheck = expLevelCheck.ElementAt(0).Selected;
+            
+            //check exp year 2
+            if (isExpCheck == true)
+            {
+                driver.FindElement(By.CssSelector("input[id='exp-1']")).Click();
+            }
+
+            //classname
+            //check automation tester option
+            driver.FindElement(By.ClassName("checkbox")).Click();
+
+            //tagname
+            //find all button
+            String button = driver.FindElements(By.TagName("button")).ToString();
+            Console.WriteLine(button);
+
+
+        }
+
+        [Test, Order(6)]
+        public void testOpaque()
+        {
+            driver.Url
+        }
+
         [TearDown]
         public void CloseTestApp()
         {
